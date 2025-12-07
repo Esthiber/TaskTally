73655021c8aeec82c222a5609a85f1d56ff237c3
Esthiber <esthiber1@gmail.com>
2025-12-07 01:20:54 -0400

    feat ✨ Implementa un nuevo componente de navegacion inferior y mejora la estructura de navegacion.

--- app/src/main/java/edu/ucne/tasktally/presentation/TaskTallyApp.kt
+++ app/src/main/java/edu/ucne/tasktally/presentation/TaskTallyApp.kt
@@ -3,7 +3,6 @@ package edu.ucne.tasktally.presentation
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -20,7 +19,6 @@ import edu.ucne.tasktally.presentation.navigation.Screen
 import edu.ucne.tasktally.presentation.navigation.TaskTallyNavHost
 import edu.ucne.tasktally.ui.theme.TaskTallyTheme
 
-@OptIn(ExperimentalMaterial3ExpressiveApi::class)
 @Composable
 fun TaskTallyApp() {
     val navController = rememberNavController()
